f2da14de206fae61d461cea228be9c3abb7553e4
sahil.kulkarni <sahil.kulkarni@selenite.co>
2019-09-06 17:48:28 +0530

    cosmetic changes done in component

--- src/pagination.js
+++ src/pagination.js
@@ -1,4 +1,4 @@
-import { Component } from 'preact';
+import { h, Component } from 'preact';
 import './style.css';
 
 export default class Pagination extends Component {
@@ -10,21 +10,13 @@ export default class Pagination extends Component {
   onNextPageClick() {
     this.props.onChangePageClick(Number(this.props.currentPageNo) + 1);
   }
+  
   goToPage(e) {
     e.preventDefault();
-    //T1473 : added textbox to jump on page provided by page no in textbox
-    // if page no sent empty  and if page no provided is greater than total pages then be on current page
-    // Developer: Samruddhi
-    // Date: 11/7/2018
     if (e.target.pageNoText.value !== '' && Math.ceil(this.props.count/10) >= e.target.pageNoText.value) {
-      // T1564 - Issues on text box near page number
-      // Developer - Shrutika Khot
-      // Date - 30/07/18
-      // comment - e.target.pageNoText.value converted to number and removed unused componentWillMount function
       this.props.onChangePageClick(Number(e.target.pageNoText.value));
     }
-    else
-    {
+    else {
       this.props.onChangePageClick(Math.ceil(this.props.count/10));
     }
   }
