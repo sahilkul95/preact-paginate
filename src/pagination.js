a2073e3d344642f103656c623f2de106cad3efff
sahil.kulkarni <sahil.kulkarni@selenite.co>
2019-09-09 10:35:08 +0530

    next/prev arrow made visible

--- src/pagination.js
+++ src/pagination.js
@@ -1,4 +1,4 @@
-import { h, Component, createElement } from 'preact';
+import { Component, createElement } from 'preact';
 import './style.css';
 
 export default class Pagination extends Component {
@@ -26,13 +26,13 @@ export default class Pagination extends Component {
     return createElement('div', {className: "pagination"},
       createElement('form', {onClick: this.goToPage.bind(this)},
         createElement('div', {},
-          createElement('span', {}, `${props.currentPageNo / Math.ceil(props.count/10)}`),
+          createElement('span', {}, `${props.currentPageNo} / ${Math.ceil(props.count/10)}`),
           createElement('button', {type: 'button', className: 'filter-btn', style: 'margin: 0 5px;', onClick: this.onPreviosPageClick.bind(this),
-            disabled: (props.currentPageNo && props.currentPageNo === 1) || (this.state.pageNoText === 1), value: '<'}),
+            disabled: (props.currentPageNo && props.currentPageNo === 1) || (this.state.pageNoText === 1)}, '<'),
           createElement('input', {type: 'number', className: 'no-spinners', name: 'pageNoText', min: '1', value: props.currentPageNo, required: true,
             style: 'width:50px; margin: 0 3px 0 3px; height: 2rem; border-bottom: 0.5px solid #bbb6ad !important; border: none;'}),
-          createElement('button', {type: 'button', class: 'filter-btn', style: 'margin: 0 5px;', onClick: this.onNextPageClick.bind(this), value: '>',
-            disabled: (props.currentPageNo === (Math.ceil(props.count/10))) || (this.state.pageNoText === (Math.ceil(props.count/10)))})
+          createElement('button', {type: 'button', class: 'filter-btn', style: 'margin: 0 5px;', onClick: this.onNextPageClick.bind(this),
+            disabled: (props.currentPageNo === (Math.ceil(props.count/10))) || (this.state.pageNoText === (Math.ceil(props.count/10)))}, '>')
         )
       )
     );
